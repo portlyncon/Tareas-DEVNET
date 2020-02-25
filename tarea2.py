4ede8ce9253409f22abb0edd93bf9f62b57ce92b
portlyncon <59870519+portlyncon@users.noreply.github.com>
2020-02-25 11:19:44 +0100

    Update tarea2.py

--- tarea2.py
+++ tarea2.py
@@ -1,17 +1,17 @@
 #Crear un programa que permita conectarse con el controlador APIC-EM de Cisco
-   # El usuario tendrá que escoger la opción que quiera (no tendrá que especificar la url a mano)
+    #El usuario tendrá que escoger la opción que quiera (no tendrá que especificar la url a mano)
     #Añadir, como mínimo, 4 funcionalidades
 
 # Import modules.
 import requests
 import json
 
-# Disable warnings.
+# Desabilitar warnings.
 requests.packages.urllib3.disable_warnings()
 
 # Variables
 apic_em_ip = "https://sandboxapicem.cisco.com/api/v1"
-tarea ="pol"
+etiqueta ="pol"
 
 
 def obtener_token(url):
@@ -46,48 +46,73 @@ def obtener_hosts_ip(token, url):
 	
     response = requests.get(url, headers=headers, verify=False).json()
     
-    #  list from response 
+    # list from response 
     hosts = response["response"]
     return hosts
        
+def obtener_serial(token, url): 
+    # Define API Call
+    api_call = "/network-device"
 
+    # Header 
+    headers = {"X-AUTH-TOKEN": token}
 
-def escojer_tarea(tarea):
+    # Combine URL, API call and parameters variables
+    url += api_call
+	
+    response = requests.get(url, headers=headers, verify=False).json()
+    
+    # list from response 
+    hosts = response["response"]
+    return hosts
+
+def escojer_tarea(etiqueta):
    
     hosts = obtener_hosts_ip(auth_token, apic_em_ip)
 
     operacion = input('''
 introduce la operacion que quieres realizar:
 1 lista IP hosts
-2 sumar
-3 multiplicar
-4 dividir
+2 lista macs
+3 conectado a
+4 serialNumber dispositivo
 ''')
     if operacion == '1':
         
-        tarea ="hostIp"
+        etiqueta ="hostIp"
         print("Listado IP hosts")
-     #passar el metode imrpimir hosts a general !!!!!!!!!!!!!!!!!
-        for host in hosts: 
-            print(host[tarea])
-        otra_opercion()
+        imprimir_lista(hosts,etiqueta)
+        
     elif operacion =='2':
 
-        tarea ="hostMac"
+        etiqueta ="hostMac"
         print("Listado mac hosts")
-        #  list of hosts
-        for host in hosts: 
-            print(host[tarea])
-        otra_opercion()
+        imprimir_lista(hosts,etiqueta)
+    
     elif  operacion =='3':
    
-        tarea ="connectedNetworkDeviceIpAddress"
-        print("Listado mac hosts")
-        #  list of hosts
-        for host in hosts: 
-            print(host[tarea])
-        otra_opercion()  
+        etiqueta ="connectedNetworkDeviceIpAddress"
+        print("Listado equipos conectados")
+        imprimir_lista(hosts,etiqueta)  
+   
+    elif operacion =='4':
 
+        hosts = obtener_serial(auth_token, apic_em_ip)
+        etiqueta ="hostname"
+        print("Listado de serial number dispositivos")
+        imprimir_lista(hosts,etiqueta)
+        
+
+
+def imprimir_lista(hosts,etiqueta):
+    for host in hosts: 
+            print(host[etiqueta]) 
+    otra_opercion()
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#for host in hosts: 
+ #   print("{ip:20} {mac:20} {type:10}".format(ip=host["hostIp"], 
+  #                                            mac=host["hostMac"], 
+   #                                           type=host["hostType"]))
 
 def otra_opercion():
     otro_calculo = input('''
@@ -96,7 +121,7 @@ introduce Y para YES o N para NO.
 ''')
 
     if otro_calculo.upper() == 'Y':
-        escojer_tarea(tarea)
+        escojer_tarea(etiqueta)
     elif otro_calculo.upper() == 'N':
         print('Adios')
     else:
@@ -107,5 +132,4 @@ auth_token = obtener_token(apic_em_ip)
 
 #Escojer que tarea quiere realizar el usuario
 
-escojer_tarea(tarea)
-    
+escojer_tarea(etiqueta)
