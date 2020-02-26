e59dddba56941a3d5abc00292e630fbeacbff152
portlyncon <59870519+portlyncon@users.noreply.github.com>
2020-02-26 14:45:14 +0100

    Update tarea2.py

--- tarea2.py
+++ tarea2.py
@@ -1,4 +1,4 @@
-    #Crear un programa que permita conectarse con el controlador APIC-EM de Cisco
+   #Crear un programa que permita conectarse con el controlador APIC-EM de Cisco
     #El usuario tendrá que escoger la opción que quiera (no tendrá que especificar la url a mano)
     #Añadir, como mínimo, 4 funcionalidades
 
@@ -10,65 +10,53 @@ import json
 requests.packages.urllib3.disable_warnings()
 
 # Variables
-apic_em_ip = "https://sandboxapicem.cisco.com/api/v1"
+direccion = "https://sandboxapicem.cisco.com/api/v1"
 etiqueta ="pol"
 
 
 def obtener_token(url):
 
-    # Define API Call to get authentication token.
-    api_call ="/ticket"
-
-    # Payload contains authentication information.
-    payload = { "username": "devnetuser", "password": "Cisco123!" }
-
-    # Header information.
+    llamada_api ="/ticket"
+    datos = { "username": "devnetuser", "password": "Cisco123!" }
     headers = {"content-type" : "application/json"}
+    url += llamada_api
 
-    # Add the API call to the URL argument.
-    url += api_call
+    response = requests.post(url, data=json.dumps(datos), headers=headers, verify=False).json()
 
-    response = requests.post(url, data=json.dumps(payload), headers=headers, verify=False).json()
-
-    # Return authentication token from respond body.
     return response["response"]["serviceTicket"]
 
 
 def obtener_hosts_ip(token, url): 
-    # Define API Call
-    api_call = "/host"
-
-    # Header 
+    
+    llamada_api = "/host"
     headers = {"X-AUTH-TOKEN": token}
 
-    # Combine URL, API call and parameters variables
-    url += api_call
-	
+    # Combinacion URL, variables API
+    url += llamada_api
+
     response = requests.get(url, headers=headers, verify=False).json()
     
-    # list from response 
+    # lista de response
     hosts = response["response"]
     return hosts
        
 def obtener_serial(token, url): 
-    # Define API Call
-    api_call = "/network-device"
-
-    # Header 
+    
+    llamada_api = "/network-device" 
     headers = {"X-AUTH-TOKEN": token}
 
-    # Combine URL, API call and parameters variables
-    url += api_call
+    # Combinacion URL, variables API
+    url += llamada_api
 	
     response = requests.get(url, headers=headers, verify=False).json()
     
-    # list from response 
+    # lisa de response 
     hosts = response["response"]
     return hosts
 
 def escojer_tarea(etiqueta):
    
-    hosts = obtener_hosts_ip(auth_token, apic_em_ip)
+    hosts = obtener_hosts_ip(auth_token, direccion)
 
     operacion = input('''
 introduce la operacion que quieres realizar:
@@ -97,41 +85,41 @@ introduce la operacion que quieres realizar:
    
     elif operacion =='4':
 
-        hosts = obtener_serial(auth_token, apic_em_ip)
+        hosts = obtener_serial(auth_token, direccion)
         etiqueta ="hostname"
         print("Listado de serial number dispositivos")
         imprimir_lista(hosts,etiqueta)
         
-
+counter = 0 
 
 def imprimir_lista(hosts,etiqueta):
     for host in hosts: 
             print(host[etiqueta]) 
-    
+   
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     #for host in hosts: 
      #   print("{ip:20} {mac:20} {type:10}".format(ip=host["hostIp"], 
       #                                           mac=host["hostMac"], 
        #                                          type=host["hostType"]))
-    otra_opercion()
+            print("Nombre equipo",hosts[counter][etiqueta],"device mac addres")
+    otra_tarea()
 
 
-def otra_opercion():
-    otro_calculo = input('''
+def otra_tarea():
+    otra_accion = input('''
 quieres realizar otra operacion?
 introduce Y para YES o N para NO.
 ''')
 
-    if otro_calculo.upper() == 'Y':
+    if otra_accion.upper() == 'Y':
         escojer_tarea(etiqueta)
-    elif otro_calculo.upper() == 'N':
+    elif otra_accion.upper() == 'N':
         print('Adios')
     else:
-        otra_opercion()
+        otra_tarea()
 
 # Assign obtained authentication token to a variable. Provide APIC-EM's URL address.
-auth_token = obtener_token(apic_em_ip)
+auth_token = obtener_token(direccion)
 
 #Escojer que tarea quiere realizar el usuario
-
 escojer_tarea(etiqueta)
