fb83c80f154a9c5fe9e203d5e8c875ff4a6db393
portlyncon <59870519+portlyncon@users.noreply.github.com>
2020-05-06 13:01:21 +0200

    Create device_info.py

---
+++ device_info.py
@@ -0,0 +1,9 @@
+ios_xe1 = {
+             "address": "10.10.20.48",
+             "netconf_port": 830,
+             "restconf_port": 830,
+             "ssh_port": 22,
+             "username": "cisco",
+             "password": "cisco_1234!",
+             "device_type": "cisco_ios"
+          }
