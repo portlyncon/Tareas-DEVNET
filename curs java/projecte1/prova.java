dba8f19c8b6471b7794b8a9167f0fa9190124310
pol <supergatchan@hotmail.com>
2021-10-19 11:43:59 +0200

    prova

---
+++ curs java/projecte1/prova.java
@@ -0,0 +1,15 @@
+package curs
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+java.projecte1;
+
+public class prova {
+
+    public static void main(String arg[]) {
+
+        System.out.println("hello wolrd");
+
+    }
+
+}
